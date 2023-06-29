e15b9df38ac7ffb357ac9e58a9431f96d4a3eb40
rheydne <rheydne@gmail.com>
2023-06-28 23:11:04 -0300

    Refatoracao TaskService

--- src/main/java/com/rheydne/todosimple/services/TaskService.java
+++ src/main/java/com/rheydne/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.rheydne.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,11 @@ public class TaskService {
         return task.orElseThrow(() -> new RuntimeException(
             "Tarefa não encontrada! id: " + id + ", Tipo: " + Task.class.getName()
         )); // Retorna a tarefa encontrada ou lança uma exceção se não for encontrada
+    }
+
+    public List<Task> findByUserId(Long userId) {
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
     }
 
     @Transactional
