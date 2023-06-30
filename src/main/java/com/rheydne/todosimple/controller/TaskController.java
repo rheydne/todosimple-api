3af9728363af789a54b94d0dfaeafb442e549495
rheydne <rheydne@gmail.com>
2023-06-30 18:14:36 -0300

    Refatoracao TaskController

--- src/main/java/com/rheydne/todosimple/controller/TaskController.java
+++ src/main/java/com/rheydne/todosimple/controller/TaskController.java
@@ -20,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rheydne.todosimple.model.Task;
 import com.rheydne.todosimple.services.TaskService;
+import com.rheydne.todosimple.services.UserService;
 
 @RestController
 @RequestMapping("/task")
@@ -29,6 +30,9 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<Task> findById(@PathVariable Long id) {
         Task obj = this.taskService.findById(id);
@@ -37,6 +41,7 @@ public class TaskController {
     
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId) {
+        userService.findById(userId);
         List<Task> objs = this.taskService.findByUserId(userId);
         return ResponseEntity.ok().body(objs);
     }
